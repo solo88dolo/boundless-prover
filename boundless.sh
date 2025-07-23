57d59b5b1341b3fef3bcd65ae2c7ec853e8b8e1f
solodolo <115883210+solo88dolo@users.noreply.github.com>
2025-07-23 13:44:43 +0700

    Update boundless.sh

--- boundless.sh
+++ boundless.sh
@@ -91,7 +91,7 @@ print_success "Repository cloned"
 
 print_step "Replacing setup script..."
 rm scripts/setup.sh
-curl -o scripts/setup.sh https://raw.githubusercontent.com/zunxbt/boundless-prover/refs/heads/main/script.sh
+curl -o scripts/setup.sh https://raw.githubusercontent.com/solo88dolo/boundless-prover/refs/heads/main/script.sh
 chmod +x scripts/setup.sh
 print_success "Setup script replaced"
 
@@ -103,11 +103,11 @@ print_step "Downloading custom configuration files..."
 # fi
 
 # Download custom broker.toml
-curl -o broker.toml https://raw.githubusercontent.com/Stevesv1/boundless/refs/heads/main/broker.toml
+curl -o broker.toml https://raw.githubusercontent.com/Solo88dolo/boundless/refs/heads/main/broker.toml
 print_success "Downloaded custom broker.toml"
 
 # Download custom compose.yml
-# curl -o compose.yml https://raw.githubusercontent.com/Stevesv1/boundless/refs/heads/main/compose.yml
+# curl -o compose.yml https://raw.githubusercontent.com/Solo88dolo/boundless/refs/heads/main/compose.yml
 # print_success "Downloaded custom compose.yml"
 
 print_step "Downloading custom Rust source files..."
@@ -124,11 +124,11 @@ if [[ -f "crates/broker/src/order_monitor.rs" ]]; then
  fi
 
 # Download custom order_monitor.rs
- curl -o crates/broker/src/order_monitor.rs https://raw.githubusercontent.com/Stevesv1/boundless/refs/heads/main/crates/broker/src/order_monitor.rs
+ curl -o crates/broker/src/order_monitor.rs https://raw.githubusercontent.com/Solo88dolo/boundless/refs/heads/main/crates/broker/src/order_monitor.rs
  print_success "Downloaded custom order_monitor.rs"
 
 # Download custom order_picker.rs
- curl -o crates/broker/src/order_picker.rs https://raw.githubusercontent.com/Stevesv1/boundless/refs/heads/main/crates/broker/src/order_picker.rs
+ curl -o crates/broker/src/order_picker.rs https://raw.githubusercontent.com/Solo88dolo/boundless/refs/heads/main/crates/broker/src/order_picker.rs
  print_success "Downloaded custom order_picker.rs"
 
 print_step "Running setup script..."
